dba57b33ca4d426a1caad8e45e4e12b91d9e26ec
NIRAJ <nirarajput6983@gmail.com>
2024-05-16 19:45:03 +0530

    16-05-2024

--- lib/view/widget.dart
+++ lib/view/widget.dart
@@ -1,173 +1,176 @@
-import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 
 class WidgetTest extends StatelessWidget {
   const WidgetTest({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      backgroundColor: CupertinoColors.tertiarySystemFill,
-      child: Center(
-          child: BackdropFilter(
-        filter: ImageFilter.blur(),
-        child: Stack(
-          children: [
-            Container(
-              height: double.infinity,
-              width: double.infinity,
-              child: Image.asset('assets/download (2).jpg'),
-            ),
-            Center(
-              child: Container(
-                height: 300,
-                width: 350,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 50, top: 40),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            '  Glass',
+    return Scaffold(
+      body: Center(
+          child: Stack(
+            children: [
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                child: Image.asset('assets/download (2).jpg',fit: BoxFit.cover,),
+              ),
+              Center(
+                child: GlassmorphicContainer(
+                  height: 300,
+                  width: 375,
+                  border: 1,
+                  borderRadius: 25,
+                  alignment: Alignment.center,
+                  linearGradient: LinearGradient(colors: [Colors.white10,Colors.white10]),
+                  borderGradient: LinearGradient(colors: [Colors.grey,Colors.black]),
+                  blur: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 50, top: 40),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              '  Glass',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25),
+                            ),
+                            SizedBox(width: 60),
+                            Text(
+                              'Home',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'About',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'Services',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'Contact',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 150),
+                          child: Text(
+                            'Morphism',
                             style: TextStyle(
-                                color: CupertinoColors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.redAccent.shade700,),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 190),
+                          child: Text(
+                            'Mockup.',
+                            style: TextStyle(
+                                color: Colors.white,
                                 fontWeight: FontWeight.bold,
                                 fontSize: 25),
                           ),
-                          SizedBox(width: 60),
-                          Text(
-                            'Home',
-                            style: TextStyle(
-                                color: CupertinoColors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 10),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            'About',
-                            style: TextStyle(
-                                color: CupertinoColors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 10),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            'Services',
-                            style: TextStyle(
-                                color: CupertinoColors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 10),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            'Contact',
-                            style: TextStyle(
-                                color: CupertinoColors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 10),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            'Login',
-                            style: TextStyle(
-                                color: CupertinoColors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 10),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 150),
-                        child: Text(
-                          'Morphism',
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: CupertinoColors.activeOrange),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 190),
-                        child: Text(
-                          'Mockup.',
-                          style: TextStyle(
-                              color: CupertinoColors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25),
+                        SizedBox(
+                          height: 15,
                         ),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        '                Loading page',
-                        style: TextStyle(
-                            color: CupertinoColors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            height: 20,
-                            width: 20,
-                            child: Text(
-                              '4K',
-                              style: TextStyle(
-                                  color: CupertinoColors.white, fontSize: 10),
-                            ),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    width: 2, color: CupertinoColors.white),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(5))),
-                          ),
-                          SizedBox(width: 5),
-                          Container(
-                            height: 20,
-                            width: 40,
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 2, left: 8),
-                              child: Text(
-                                'PSD',
-                                style: TextStyle(
-                                    color: CupertinoColors.white, fontSize: 10),
+                        Text(
+                          '                Loading page',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10),
+                              child: Container(
+                                height: 20,
+                                width: 20,
+                                child: Text(
+                                  '4K',
+                                  style: TextStyle(
+                                      color:Colors.white, fontSize: 10),
+                                ),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        width: 2, color: Colors.white,),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5))),
                               ),
                             ),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    width: 2, color: CupertinoColors.white),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(5))),
-                          ),
-                        ],
-                      ),
-                    ],
+                            SizedBox(width: 5),
+                            Container(
+                              height: 20,
+                              width: 40,
+                              child: Padding(
+                                padding: const EdgeInsets.only(top: 2, left: 8),
+                                child: Text(
+                                  'PSD',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10),
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 2, color: Colors.white,),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5))),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
+
                 ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    color: CupertinoColors.black),
               ),
-            ),
-          ],
-        ),
-      )
+            ],
+          )
           // child: Container(height: 400,width: 350,child: Image.asset('assets/widget.jpg',fit: BoxFit.cover,)),
 
           ),
